d50af72aba64cb9943eff2df220b774b137b24b5
Steven Sinha <stevensinha329@gmail.com>
2021-11-06 01:16:59 -0400

    naive login screen implementation

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ import {
     HomeWrapper,
     RegisterScreen,
     Statusbar,
-    WorkspaceScreen
+    WorkspaceScreen,
+    LoginScreen,
 } from './components'
 /*
     This is our application's top-level component.
@@ -31,6 +32,7 @@ const App = () => {
                         <Route path="/" exact component={HomeWrapper} />
                         <Route path="/register/" exact component={RegisterScreen} />
                         <Route path="/top5list/:id" exact component={WorkspaceScreen} />
+                        <Route path ="/login/" exact component={LoginScreen} />
                     </Switch>
                     <Statusbar />
                 </GlobalStoreContextProvider>
