e9b11b76093b636745a066d5df5fb54a24249648
stevenwsinha <stevensinha329@gmail.com>
2021-11-26 15:18:47 -0500

    update server routing

--- server/routes/top5lists-router.js
+++ server/routes/top5lists-router.js
@@ -4,13 +4,19 @@ const Top5ListController = require('../controllers/top5list-controller')
 const UserController = require('../controllers/user-controller')
 const router = express.Router()
 
-router.post('/top5list', auth.verify, Top5ListController.createTop5List)
-router.put('/top5list/:id', auth.verify, Top5ListController.updateTop5List)
-router.delete('/top5list/:id', auth.verify, Top5ListController.deleteTop5List)
-router.get('/top5list/:id', auth.verify, Top5ListController.getTop5ListById)
-router.get('/top5lists', auth.verify, Top5ListController.getTop5Lists)
-router.get('/top5listpairs', auth.verify, Top5ListController.getTop5ListPairs)
+// SINGLE LIST CRUD OPERATIONS
+router.post('/home/top5list/:id', auth.verify, Top5ListController.createTop5List)
+router.put('/home/top5list/:id', auth.verify, Top5ListController.updateTop5List)
+router.delete('home//top5list/:id', auth.verify, Top5ListController.deleteTop5List)
+router.get('home//top5list/:id', auth.verify, Top5ListController.getTop5ListById)
 
+// MULTI LIST RETRIEVAL 
+router.get('/home/top5lists', auth.verify, Top5ListController.getTop5Lists)
+router.get('/home/:username', Top5ListController.getTop5ListPairs)
+router.get('home/all', Top5ListController.getTop5ListPairs)
+// router.get('home/community', CommunityController.getCommunityLists)
+
+// USER CRUD OPERATIONS
 router.get('/logout', UserController.logoutUser)
 router.post('/register', UserController.registerUser)
 router.get('/loggedIn', UserController.getLoggedIn)
