db1f6014e81e7c4dff087d496bba52e7189e780f
stevenwsinha <stevensinha329@gmail.com>
2021-11-27 12:31:22 -0500

    save draft of listcard layout

--- client/src/components/ListCard.js
+++ client/src/components/ListCard.js
@@ -1,14 +1,13 @@
 import { useContext, useState } from 'react'
 import { GlobalStoreContext } from '../store'
-import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import ListItem from '@mui/material/ListItem';
 import IconButton from '@mui/material/IconButton';
-import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+import Typography from '@mui/material/Typography';
 
 import Grid from '@mui/material/Grid';
 
@@ -37,80 +36,143 @@ function ListCard(props) {
         store.markListForDeletion(id);
     }
     return (
-        <ListItem
-        id={list._id}
-        className={'list-card'}
-        key={list._id}
-        sx={{ marginTop: '0px', display: 'flex', p: 1}}
-        style={{
-            fontSize: '12pt',
-            width: '98%',
-            left: '1%'
-        }}
+    <ListItem
+    id={list._id}
+    className={'list-card'}
+    key={list._id}
+    sx={{ marginTop: '0px', display: 'flex', p: 1}}
+    style={{
+        fontSize: '12pt',
+        minHeight: 90,
+        maxHeight: 90,
+        borderStyle: 'solid',
+        borderWidth: 3,
+        borderColor: 'red',
+        width: '98%',
+        left: '1%'
+    }}
     >
-        <Grid container spacing={2}>
-            <Grid item container xs={8}>
-                <Grid item xs={12}>
-                    {list.name}
+        <Box sx={{ 
+            p: 0, 
+            flexGrow: 3,  
+            height: 80,
+            borderStyle: 'solid',
+            borderWidth: 3,
+            borderColor: 'blue',}}> 
+            <Grid container direction='column' spacing={0} sx={{
+                height: 78,
+                borderStyle: 'solid',
+                borderWidth: 3,
+                borderColor: 'green',
+            }}>
+                <Grid item sx={{
+                     height: 32,
+                     borderStyle: 'solid',
+                     borderWidth: 3,
+                     borderColor: 'pink',
+                }}>
+                    first grid item
                 </Grid>
-                <Grid item xs={12}>
-                    {list.owner}
+                <Grid item sx={{
+                     height: 20,
+                     borderStyle: 'solid',
+                     borderWidth: 3,
+                     borderColor: 'pink',
+                }}>
+                    second grid item
                 </Grid>
-                <Grid item xs={12}>
-                    {list.isPublished}
+                <Grid item sx={{
+                     height: 20,
+                     borderStyle: 'solid',
+                     borderWidth: 3,
+                     borderColor: 'pink',
+                }}>
+                    third grid item
+                </Grid>
+            </Grid> 
+        </Box>
+        <Box sx={{ 
+        p: 0, 
+        flexGrow: 1,
+        height: 80,
+        borderStyle: 'solid',
+        borderWidth: 3,
+        borderColor: 'yellow',}}>
+            <Box sx={{ 
+            p: 0, 
+            flexGrow: 1,
+            height: 40,
+            borderStyle: 'solid',
+            borderWidth: 3,
+            borderColor: 'orange',}}>
+            <Grid container direction='row' spacing={0} sx={{
+                  borderStyle: 'solid',
+                  borderWidth: 3,
+                  height: 40,
+                  borderColor: 'green',
+            }}>
+                <Grid item xs={4} sx={{
+                    borderStyle: 'solid',
+                    borderWidth: 3,
+                    height: 40,
+                    borderColor: 'blue',
+                }}>
+                    First Grid Item
+                </Grid>
+                <Grid item xs={4} sx={{
+                    borderStyle: 'solid',
+                    borderWidth: 3,
+                    height: 40,
+                    borderColor: 'blue',
+                }}>
+                    Second Grid Item
+                </Grid>
+                <Grid item xs={4} sx={{
+                    borderStyle: 'solid',
+                    borderWidth: 3,
+                    height: 40,
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    borderColor: 'blue',
+                }}>
+                    Third Grid Item
                 </Grid>
             </Grid>
-            <Grid item container xs={4}>
-                <Grid item xs={4}>
-                <IconButton
-                        size="small"
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        sx={{ mr: 1 }}
-                  >
-                    <ThumbUpIcon />
-                  </IconButton>
-                    {list.likes.length}
+            </Box> 
+            <Box sx={{ 
+            p: 0, 
+            flexGrow: 1,
+            height: 30,
+            borderStyle: 'solid',
+            borderWidth: 3,
+            borderColor: 'black',}}>
+                <Grid container direction='row' spacing={0} sx={{
+                  borderStyle: 'solid',
+                  borderWidth: 3,
+                  height: 30,
+                  borderColor: 'pink',
+            }}>
+                <Grid item xs={8} sx={{
+                    borderStyle: 'solid',
+                    borderWidth: 3,
+                    height: 30,
+                    borderColor: 'blue',
+                }}>
+                    First Grid Item
                 </Grid>
-                <Grid item xs={4}>
-                <IconButton
-                        size="small"
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        sx={{ mr: 1 }}
-                  >
-                    <ThumbDownIcon />
-                  </IconButton>
-                    {list.dislikes.length}
-                </Grid>
-                <Grid item xs={4}>
-                    <IconButton
-                        size="small"
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </Grid>
-                <Grid item xs={8}>
-                    Views: {list.views}
-                </Grid>
-                <Grid item xs={4}>
-                <IconButton
-                        size="small"
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        sx={{ mr: 2 }}
-                  >
-                    <ExpandMoreIcon />
-                  </IconButton>
+                <Grid item xs={4} sx={{
+                    borderStyle: 'solid',
+                    borderWidth: 3,
+                    height: 30,
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    borderColor: 'blue',
+                }}>
+                    Second Grid Item
                 </Grid>
             </Grid>
-        </Grid>
+            </Box>   
+        </Box>   
     </ListItem>
     );
 }
