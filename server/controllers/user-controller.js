818a21fb7c7cada16e649d8d2ba90e520b1f8cf8
stevenwsinha <stevensinha329@gmail.com>
2021-11-28 09:25:48 -0500

    fix auth controllers not returning username

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -9,6 +9,7 @@ getLoggedIn = async (req, res) => {
             return res.status(200).json({
                 loggedIn: true,
                 user: {
+                    username: loggedInUser.username,
                     firstName: loggedInUser.firstName,
                     lastName: loggedInUser.lastName,
                     email: loggedInUser.email
@@ -142,6 +143,7 @@ loginUser = async (req, res) => {
         }).status(200).json({
             success: true,
             user: {
+                username: existingUser.username,
                 firstName: existingUser.firstName,
                 lastName: existingUser.lastName,
                 email: existingUser.email
